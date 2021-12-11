f9475ae0c0cc80062de99c5fb173a27598d5babb
Ryan Cabral <cabral.reis23@gmail.com>
2021-12-10 23:52:36 -0300

    Sixth commit (Codigo SQL sem FK e Relacionamento)

--- Ryan_Cabral_Reis_T3_AtividadeMod_3/Modelagem_de_dados/ModeloFisico_CodigoSQL.sql
+++ Ryan_Cabral_Reis_T3_AtividadeMod_3/Modelagem_de_dados/ModeloFisico_CodigoSQL.sql
@@ -3,12 +3,20 @@ create database AgenciaViagens;
 use AgenciaViagens;
 
 create table Cliente(
-id_cliente int primary key not null,
+id_cliente int identity (1,1) primary key not null,
 nome_cliente char(50) not null,
 email char(50) not null,
 senha char(8) not null,
-dataNasc char(12) not null,
-estadoOrigem char(60) not null,
-dataIda char(12) not null,
-dataVolta char(12) not null
+data_Nasc char(12) not null,
+estado_Origem char(60) not null,
+data_Ida char(12) not null,
+data_Volta char(12) not null
+);
+
+create table Destino(
+id_destino int identity (1,1) primary key not null,
+estado_Destino char(60) not null,
+cidade_Destino char(60) not null,
+pais_Destino char(60) not null,
+preco decimal not null
 );
