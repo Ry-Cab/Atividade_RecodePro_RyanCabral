45a1cfab007bdbe332661338b6a792c7b7f00e83
Ryan Cabral <cabral.reis23@gmail.com>
2021-12-11 02:16:05 -0300

    7 commit (MVC funcionando)

---
+++ Ryan_Cabral_Reis_T3_AtividadeMod_3/AgenciaViagensMVC/AgenciaViagensMVC/Models/Destino.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgenciaViagensMVC.Models
+{
+    public class Destino
+    {
+        [Key]
+        public int IdDestino { get; set; }
+
+        [Required(ErrorMessage ="Digite uma cidade")]
+        [StringLength(50)]
+        public string CidadeDestino { get; set; }
+
+        [Required(ErrorMessage = "Digite um estado")]
+        [StringLength(50)]
+        public string EstadoDestino { get; set; }
+
+        [Required(ErrorMessage = "Digite um país")]
+        [StringLength(50)]
+        public string PaisDestino { get; set; }
+    }
+}
---
+++ Ryan_Cabral_Reis_T3_AtividadeMod_3/AgenciaViagensMVC/AgenciaViagensMVC/Models/DestinoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgenciaViagensMVC.Models
+{
+    public class DestinoContext : DbContext
+    {
+        public DestinoContext(DbContextOptions<DestinoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Destino> Destinos { get; set;}
+
+    }
+}
